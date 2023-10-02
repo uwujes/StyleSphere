19c70659b397a564e4ffeedbea9c3ce511f3bd54
uwujes <salazarje@appstate.edu>
2023-10-02 15:29:02 -0400

    Activities for buttons created, text not showing.

--- app/src/main/java/com/example/stylesphere/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/stylesphere/ui/notifications/NotificationsFragment.java
@@ -1,21 +1,29 @@
 package com.example.stylesphere.ui.notifications;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.example.stylesphere.FAQ;
+import com.example.stylesphere.R;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.stylesphere.StyleTips;
 import com.example.stylesphere.databinding.FragmentNotificationsBinding;
 
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
 
+    private Button faq;
+    private Button tips;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         NotificationsViewModel notificationsViewModel =
@@ -25,8 +33,35 @@ public class NotificationsFragment extends Fragment {
         View root = binding.getRoot();
 
         final TextView textView = binding.textNotifications;
+        faq = root.findViewById(R.id.faq);
+        tips = root.findViewById(R.id.stlyeTips);
+
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        faq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFaq();
+            }
+        });
+        tips.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openStyleTips();
+            }
+        });
+
         return root;
+    }
+
+    public void openFaq() {
+        Intent intent = new Intent(requireContext(), FAQ.class);
+        startActivity(intent);
+    }
+
+    public void openStyleTips() {
+        Intent intent = new Intent(requireContext(), StyleTips.class);
+        startActivity(intent);
     }
 
     @Override
@@ -34,4 +69,5 @@ public class NotificationsFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
 }
