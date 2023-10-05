0e95d911b3f43b1b25348fbe4af4d79b11c0bbb6
uwujes <salazarje@appstate.edu>
2023-10-05 15:14:28 -0400

    Deleted content_scrolling.xml, implemented tips and faq. Can add if needed.

--- app/src/main/java/com/example/stylesphere/StyleTips.java
+++ app/src/main/java/com/example/stylesphere/StyleTips.java
@@ -2,14 +2,12 @@ package com.example.stylesphere;
 
 import android.os.Bundle;
 
-import com.google.android.material.appbar.CollapsingToolbarLayout;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.stylesphere.databinding.ActivityStyleTipsBinding;
 
@@ -17,17 +15,20 @@ public class StyleTips extends AppCompatActivity {
 
     private ActivityStyleTipsBinding binding;
 
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityStyleTipsBinding.inflate(getLayoutInflater());
+        View root = binding.getRoot();
         setContentView(binding.getRoot());
 
         Toolbar toolbar = binding.toolbar;
         setSupportActionBar(toolbar);
-        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
-        toolBarLayout.setTitle(getTitle());
+        textView = root.findViewById(R.id.textView);
+        textView.setMovementMethod(new ScrollingMovementMethod());
+
 
 //        FloatingActionButton fab = binding.fab;
 //        fab.setOnClickListener(new View.OnClickListener() {
