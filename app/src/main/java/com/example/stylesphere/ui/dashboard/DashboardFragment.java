eccf901836ad4b8f2394619cdcd01fa5f84848ab
uwujes <salazarje@appstate.edu>
2023-10-12 14:23:08 -0400

    ui for generating outfit done

--- app/src/main/java/com/example/stylesphere/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/stylesphere/ui/dashboard/DashboardFragment.java
@@ -4,17 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import com.example.stylesphere.R;
+
 
 import com.example.stylesphere.databinding.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
+    private TextView textView;
+    private ImageView clothing1;
+    private ImageView clothing2;
+    private View weatherView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,8 +31,13 @@ public class DashboardFragment extends Fragment {
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        textView = root.findViewById(R.id.text_dashboard);
+        weatherView = root.findViewById(R.id.weather);
+        clothing1 = root.findViewById(R.id.clothing1);
+        clothing2 = root.findViewById(R.id.clothing2);
+
+
+
         return root;
     }
 
