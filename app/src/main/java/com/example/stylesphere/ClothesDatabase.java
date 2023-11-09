20362428c5a11ed1e8c377f6b7d35ae46f10d5f7
uwujes <salazarje@appstate.edu>
2023-11-09 15:38:11 -0500

    Added sign out feature. Made clothing database to retrieve urls.

---
+++ app/src/main/java/com/example/stylesphere/ClothesDatabase.java
@@ -0,0 +1,5 @@
+package com.example.stylesphere;
+
+public class ClothesDatabase{
+
+}
--- app/src/main/java/com/example/stylesphere/FAQ.java
+++ app/src/main/java/com/example/stylesphere/FAQ.java
@@ -2,10 +2,6 @@ package com.example.stylesphere;
 
 import android.os.Bundle;
 
-import com.google.android.material.appbar.CollapsingToolbarLayout;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
