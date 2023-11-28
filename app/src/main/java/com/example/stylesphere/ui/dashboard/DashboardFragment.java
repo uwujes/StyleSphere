bb925272445c9ba31140cdf98e4545025f22f7ba
uwujes <salazarje@appstate.edu>
2023-11-28 13:10:15 -0500

    Finished app.

--- app/src/main/java/com/example/stylesphere/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/stylesphere/ui/dashboard/DashboardFragment.java
@@ -28,6 +28,7 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.stylesphere.ClothesDatabase;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -49,10 +50,13 @@ import java.util.Locale;
 
 
 import com.example.stylesphere.databinding.FragmentDashboardBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -64,11 +68,15 @@ public class DashboardFragment extends Fragment {
     private TextView location;
     private TextView status;
     private TextView temp;
+    double temperature;
+    private String mainStatus;
     String currentDate = new SimpleDateFormat("MM-dd-yyyy", Locale.getDefault()).format(new Date());
     private TextView date;
     private ImageView clothing1;
     private ImageView clothing2;
     private Button getWeather;
+    private Button generateOutfit;
+    private ClothesDatabase clothesDatabase;
     FusedLocationProviderClient mFusedLocationClient;
     int PERMISSION_ID = 44;
     private ActivityResultLauncher<String> requestPermissionLauncher;
@@ -78,6 +86,10 @@ public class DashboardFragment extends Fragment {
     private StorageReference mStorageRef = FirebaseStorage.getInstance().getReference("defaultSuggested");
     private DatabaseReference mDatabaseRef = FirebaseDatabase.getInstance().getReference();
     private Uri mImageUri1;
+    private FirebaseAuth mAuth =  FirebaseAuth.getInstance();
+
+
+    private boolean weatherInfo = false;
 
 
 
@@ -118,6 +130,7 @@ public class DashboardFragment extends Fragment {
         clothing1 = root.findViewById(R.id.clothing1);
         clothing2 = root.findViewById(R.id.clothing2);
         getWeather = root.findViewById(R.id.weatherButton);
+        generateOutfit = root.findViewById(R.id.outfit);
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this.getActivity());
 
@@ -132,6 +145,13 @@ public class DashboardFragment extends Fragment {
                     // Permission is already granted, proceed with your task
                     getLastLocation();
                 }
+            }
+        });
+
+        generateOutfit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                outfitGeneration();
             }
         });
 
@@ -279,9 +299,11 @@ public class DashboardFragment extends Fragment {
                         public void run() {
                             try {
                                 location.setText(jsonResponse.getString("name"));
-                                double temperature = jsonResponse.getJSONObject("main").getDouble("temp");
+                                temperature = jsonResponse.getJSONObject("main").getDouble("temp");
                                 temp.setText(temperature + " °F");
                                 status.setText(jsonResponse.getJSONArray("weather").getJSONObject(0).getString("description"));
+                                weatherInfo = true;
+                                mainStatus = jsonResponse.getJSONArray("weather").getJSONObject(0).getString("main");
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
@@ -299,6 +321,163 @@ public class DashboardFragment extends Fragment {
         networkThread.start();
     }
 
+    public void outfitGeneration(){
+
+        if(weatherInfo == false)
+        {
+            Toast.makeText(this.getActivity(), "Please click get weather first" , Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(this.getActivity(), "Generating....", Toast.LENGTH_SHORT).show();
+        }
+
+        FirebaseUser firebaseUser = mAuth.getCurrentUser();
+        String userId = firebaseUser.getUid();
+        clothesDatabase = new ClothesDatabase(userId);
+
+        //If above 70 degrees choose skirt/short with short sleeve or just dress
+        String[] choices = new String[]{"skirt", "short", "dress"};
+
+        if(temperature >= 70)
+        {
+            int index = (int) (Math.random() * choices.length);
+            switch (choices[index]) {
+                case "skirt":
+                    clothesDatabase.skirtsSelector(new ClothesDatabase.ClothesCallback() {
+                        @Override
+                        public void onCallback(String url) {
+                            // Use the URL here or pass it to another method
+                            Picasso.get().load(url).into(clothing2);
+
+                        }
+                    });
+
+                    clothesDatabase.shortSSelector(new ClothesDatabase.ClothesCallback() {
+                        @Override
+                        public void onCallback(String url) {
+                            Picasso.get().load(url).into(clothing1);
+                        }
+                    });
+                case "short":
+                    clothesDatabase.shortsSelector(new ClothesDatabase.ClothesCallback() {
+                        @Override
+                        public void onCallback(String url) {
+                            // Use the URL here or pass it to another method
+                            Picasso.get().load(url).into(clothing2);
+
+                        }
+                    });
+                    clothesDatabase.shortSSelector(new ClothesDatabase.ClothesCallback() {
+                        @Override
+                        public void onCallback(String url) {
+                            Picasso.get().load(url).into(clothing1);
+                        }
+                    });
+                case "dress":
+                    clothesDatabase.dressSelector(new ClothesDatabase.ClothesCallback() {
+                        @Override
+                        public void onCallback(String url) {
+                            // Use the URL here or pass it to another method
+                            Picasso.get().load(url).into(clothing1);
+
+                        }
+                    });
+                    break;
+            }
+
+        }
+        //60-70 long sleeve/(skirt/short) but recommend a sweater by toast
+        if(temperature >= 60 && temperature < 70)
+        {
+            String[] choices2 = new String[]{"skirt", "short"};
+            int index = (int) (Math.random() * choices.length);
+            clothesDatabase.longSSelector(new ClothesDatabase.ClothesCallback() {
+                @Override
+                public void onCallback(String url) {
+                    // Use the URL here or pass it to another method
+                    Picasso.get().load(url).into(clothing1);
+
+                }
+            });
+            switch (choices2[index]){
+                case "skirt":
+                    clothesDatabase.skirtsSelector(new ClothesDatabase.ClothesCallback() {
+                        @Override
+                        public void onCallback(String url) {
+                            // Use the URL here or pass it to another method
+                            Picasso.get().load(url).into(clothing2);
+
+                        }
+                    });
+
+                case "short":
+                    clothesDatabase.shortsSelector(new ClothesDatabase.ClothesCallback() {
+                        @Override
+                        public void onCallback(String url) {
+                            // Use the URL here or pass it to another method
+                            Picasso.get().load(url).into(clothing2);
+
+                        }
+                    });
+                    break;
+            }
+
+        }
+        //50-60 pants/short sleeve but recommend a sweater by toast
+        if(temperature >= 50 && temperature < 60)
+        {
+            clothesDatabase.shortSSelector(new ClothesDatabase.ClothesCallback() {
+                @Override
+                public void onCallback(String url) {
+                    // Use the URL here or pass it to another method
+                    Picasso.get().load(url).into(clothing1);
+
+                }
+            });
+            clothesDatabase.pantsSelector(new ClothesDatabase.ClothesCallback() {
+                @Override
+                public void onCallback(String url) {
+                    // Use the URL here or pass it to another method
+                    Picasso.get().load(url).into(clothing2);
+
+                }
+            });
+        }
+        //If between if below 50 choose pants with long sleeve
+        if(temperature < 50)
+        {
+            clothesDatabase.pantsSelector(new ClothesDatabase.ClothesCallback() {
+                @Override
+                public void onCallback(String url) {
+                    // Use the URL here or pass it to another method
+                    Picasso.get().load(url).into(clothing2);
+
+                }
+            });
+            clothesDatabase.longSSelector(new ClothesDatabase.ClothesCallback() {
+                @Override
+                public void onCallback(String url) {
+                    // Use the URL here or pass it to another method
+                    Picasso.get().load(url).into(clothing1);
+
+                }
+            });
+        }
+
+
+        
+        if(mainStatus == "Rain")
+        {
+            Toast.makeText(this.getActivity(), "Pair this outfit with rainboots or a raincoat!", Toast.LENGTH_LONG).show();
+        }
+        if(mainStatus == "Snow")
+        {
+            Toast.makeText(this.getActivity(), "Pair this outfit with snowboots!", Toast.LENGTH_LONG).show();
+        }
+
+
+
+    }
 
 
 }
